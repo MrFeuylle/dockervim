3e0c1937b5e4e990bf22061a6e0b794ba1f4fd05
Axel Giguaire <113340829+MrFeuylle@users.noreply.github.com>
2024-04-15 20:21:40 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,4 +1,4 @@
-sed -i "/alias nvi='docker run -it -v \$PWD:/root/projet myimgnvim /root/entrypoint.sh'/d" ~/.zshrc
-sed -i "alias eoc='/tmp/dockervim/remove.sh'/d" ~/.zshrc
+sed -i "/alias nvi='docker run -it -v \$PWD:\/root\/projet myimgnvim \/root\/entrypoint.sh'/d" ~/.zshrc
+sed -i "/alias eoc='\/tmp\/dockervim\/remove.sh'/d" ~/.zshrc
 docker image rm -f myimgnvim 
 docker system prune -f
