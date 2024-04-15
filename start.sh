abd94847affdca6c25e0931f36a0da44285ad4f9
Axel Giguaire <113340829+MrFeuylle@users.noreply.github.com>
2024-04-15 18:16:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ cd /tmp/dockervim
 docker build -t myimgnvim .
 cat <<'EOF' >> ~/.zshrc
 alias nvi='docker run -it -v $PWD:/root/projet myimgnvim /root/entrypoint.sh'
-alias eoc='docker image rm -f 6f1a0bab707e && docker system prune -f'
+alias eoc='docker image rm -f myimgnvim && docker system prune -f'
 EOF
