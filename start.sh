1fb25ea4a17c95467a0b64505dca15145d2a02ba
Axel GIGUAIRE <agiguair@e2r10p13.clusters.42paris.fr>
2024-04-15 15:48:24 +0200

    fin

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 git clone git@github.com:MrFeuylle/dockervim.git
 cd dockervim
 docker build -t myimgnvim .
-alias nvi "docker run -it -v $PWD:/root/projet myimgnvim ./entrypoint.sh"
+alias nvi="docker run -it -v $PWD:/root/projet myimgnvim ./entrypoint.sh"
